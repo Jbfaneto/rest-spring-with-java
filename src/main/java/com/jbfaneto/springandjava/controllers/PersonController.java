2f947d9b9b8479eceba855c5529422eb869543e2
Jbfaneto <jbfaneto@gmail.com>
2023-11-03 11:54:09 -0300

    Refectored PersonController to deal with ResponseEntities

--- src/main/java/com/jbfaneto/springandjava/controllers/PersonController.java
+++ src/main/java/com/jbfaneto/springandjava/controllers/PersonController.java
@@ -4,8 +4,11 @@ import com.jbfaneto.springandjava.models.Person;
 import com.jbfaneto.springandjava.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,25 +19,32 @@ public class PersonController {
 
     @GetMapping(value="/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable Long id) {
-        return service.findById(id);
+    public ResponseEntity<Person> findById(@PathVariable Long id) {
+        Person person = service.findById(id);
+        return ResponseEntity.ok().body(person);
     }
     @GetMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
-        return service.findAll();
+    public ResponseEntity<List<Person>> findAll(){
+        List<Person> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 
     @PostMapping(value="/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person){
-        return service.create(person);
+    public ResponseEntity<Person> create(@RequestBody Person person){
+        Person newPerson = service.create(person);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newPerson.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
     }
-    @RequestMapping(value="/{id}", method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@PathVariable Long id, @RequestBody Person person){
-        return service.update(id, person);
+    @PutMapping(value="/{id}",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Person person){
+        Person obj = service.update(id, person);
+        return ResponseEntity.ok().body(obj);
     }
 
     @DeleteMapping(value="/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<?> delete(@PathVariable Long id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
